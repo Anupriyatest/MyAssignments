640f21ee90c06dbef35c788c013dfc7ee3924b84
Anupriyatest <anupriyarajld@gmail.com>
2025-11-19 21:10:14 +0530

    Add files via upload
    Abstract

---
+++ Assignment/week3/day3/DatabseConnection.java
@@ -0,0 +1,9 @@
+package assignment.week3.day3;
+
+public interface DatabseConnection {
+	
+	public void connect();
+	public void disconnect();
+	public void executeUpdate();
+
+}
---
+++ Assignment/week3/day3/MySqlConnection.java
@@ -0,0 +1,9 @@
+package assignment.week3.day3;
+
+public abstract class MySqlConnection implements DatabseConnection {
+	
+	public void executeQuery() {
+		System.out.println("Execute the Sql query to update");
+	}
+
+}
