25c9143949ee3b14b01407665c5a8bfb77ac0ab3
Anupriyatest <anupriyarajld@gmail.com>
2025-11-19 20:53:08 +0530

    week3/day3

--- week3/day2/day3/MySqlConnection.java
+++
@@ -1,9 +0,0 @@
-package assignment.week3.day3;
-
-public abstract class MySqlConnection implements DatabseConnection {
-	
-	public void executeQuery() {
-		System.out.println("Execute the Sql query to update");
-	}
-
-}
